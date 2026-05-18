64ee4eef8b9d8a22f817c33cb84d47f99fd3aaf3
sindy918 <sindybathula98@gmail.com>
2026-05-18 19:12:49 +0530

    Add files via upload

--- GigFlow – Smart Leads Dashboard/backend/backend-clean/src/index.ts
+++ GigFlow – Smart Leads Dashboard/backend/backend-clean/src/index.ts
@@ -61,6 +61,7 @@ app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+
+app.listen(PORT as number, "0.0.0.0", () => {
+  console.log(`Server running in production mode on port ${PORT}`);
 });
